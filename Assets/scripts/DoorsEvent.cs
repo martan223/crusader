a619a423992fa22df3eec52e6c8475568e7181d5
martan223 <martin.silpoch@gmail.com>
2017-02-03 21:52:51 +0100

    little change

---
+++ Assets/scripts/DoorsEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace Assets.scripts
+{
+    class DoorsEvent : MonoBehaviour
+    {
+        string nextScene;
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            NextScene ns = Scene_Controller.scene.Layer2.Find(p => p.Name == gameObject.name)as NextScene;
+            nextScene = ns.nextScene;
+            Scene_Controller.scrtransition = new FadeOut(10f, 0.1F, nextScene, true);
+            Scene_Controller.transition = true;
+        }
+    }
+}
