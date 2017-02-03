50d7924d8583abcfa7e5102331464d33541c3f41
martan223 <martin.silpoch@gmail.com>
2017-02-03 21:54:35 +0100

    newly updated

---
+++ Assets/scripts/FadeOut.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.scripts
+{
+    class FadeOut : ScreenTransition
+    {
+        string scenePath;
+        bool nextIn;
+        public FadeOut(float Duration, float Speed, string SceneInPath,bool nextIn)
+        {
+            this.Speed = Speed;
+            this.Duration = Duration;
+            Remaining = 0;
+            scenePath = SceneInPath;
+            this.nextIn = nextIn;
+        }
+        public override void Update()
+        {
+            Remaining += Speed;
+            Debug.Log(Remaining / Duration);
+            if (Remaining < Duration / 2)
+                GameObject.Find("Main Camera").GetComponent<GUITexture>().color = new Color(0, 0, 0, Remaining / Duration);
+            else
+            {
+                GameObject.Find("Main Camera").GetComponent<GUITexture>().color = new Color(0, 0, 0, 0.5f);
+                Scene_Controller.scene.Load(scenePath);
+                //Scene_Controller.transition = false;
+                if(nextIn)
+                    Scene_Controller.scrtransition = new FadeIn(10f, 0.1F, "test_scene");
+            }
+        }
+    }
+}
---
+++ Assets/scripts/NextScene.cs
@@ -0,0 +1,21 @@
+﻿using Assets.scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class NextScene : GameItem{
+    public string nextScene;
+	// Use this for initialization
+    public NextScene() { }
+    public NextScene(GameObject go, string path)
+    {
+        position = go.transform.position;
+        scale = go.transform.localScale;
+        Name = go.name;
+        nextScene = path;
+    }
+	// Update is called once per frame
+	void Update () {
+	    	
+	}
+}
